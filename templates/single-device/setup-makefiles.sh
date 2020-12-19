ab77884b8ee81b765726b152a51a81fb0569cfaf
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 03:29:25 +0200

    Remove branding from script variables
    No need to force branding down everyone's throat,
    most of the other android forks using these scripts are
    rebranding the variables anyway.
    Let's reduce fragmentation.
    
    Change-Id: Ie9c223d1a42728076f45b40a11cd0820010db8b1

--- templates/single-device/setup-makefiles.sh
+++ templates/single-device/setup-makefiles.sh
@@ -27,9 +27,9 @@ INITIAL_COPYRIGHT_YEAR=**** FILL IN COPYRIGHT YEAR ****
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/tools/extract-utils/extract_utils.sh
+HELPER="$ANDROID_ROOT"/tools/extract-utils/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -37,7 +37,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
 
 # Copyright headers and guards
 write_headers
