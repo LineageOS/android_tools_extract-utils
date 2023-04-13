d5ce11392c8987c139e3bbb56349af07a742e5be
Michael Bestas <mkbestas@gmail.com>
2023-04-13 16:38:34 +0300

    extract_firmware: Handle renamed firmware files
    This allows to extract firmware from OTA images
    instead of factory on FP4
    
    Change-Id: Ie52e85f63a3a6e48551a0cd8b6899cc542444587

--- extract_utils.sh
+++ extract_utils.sh
@@ -1916,7 +1916,11 @@ function extract_firmware() {
         if [ ! -d "$OUTPUT_DIR" ]; then
             mkdir -p "$OUTPUT_DIR"
         fi
-        cp "$SRC/$SRC_FILE" "$OUTPUT_DIR/$DST_FILE"
+        if [ -f "$SRC/$SRC_FILE" ]; then
+            cp "$SRC/$SRC_FILE" "$OUTPUT_DIR/$DST_FILE"
+        else
+            cp "$SRC/$DST_FILE" "$OUTPUT_DIR/$DST_FILE"
+        fi
         chmod 644 "$OUTPUT_DIR/$DST_FILE"
     done
 }
