687d74f9e16988afd87ce09dbc61fd09d3cac51f
LuK1337 <priv.luk@gmail.com>
2023-11-30 18:11:51 +0100

    extract-utils: Add support for EROFS extraction
    Change-Id: I08a83851bb2f35b68291f1df2fb961c6fb9c82bc

--- extract_utils.sh
+++ extract_utils.sh
@@ -1614,7 +1614,9 @@ function prepare_images() {
             echo "Extracting "$PARTITION""
             local IMAGE="$SRC"/"$PARTITION".img
             if [ -f "$IMAGE" ]; then
-                if [[ $(file -b "$IMAGE") == Linux* ]]; then
+                if [[ $(file -b "$IMAGE") == EROFS* ]]; then
+                    fsck.erofs --extract="$DUMPDIR"/"$PARTITION" "$IMAGE"
+                elif [[ $(file -b "$IMAGE") == Linux* ]]; then
                     extract_img_data "$IMAGE" "$DUMPDIR"/"$PARTITION"
                 elif [[ $(file -b "$IMAGE") == Android* ]]; then
                     "$SIMG2IMG" "$IMAGE" "$DUMPDIR"/"$PARTITION".raw
@@ -1953,7 +1955,9 @@ function generate_prop_list_from_image() {
 
     mkdir -p "$image_dir"
 
-    if [[ $(file -b "$image_file") == Linux* ]]; then
+    if [[ $(file -b "$image_file") == EROFS* ]]; then
+        fsck.erofs --extract="$image_dir" "$image_file"
+    elif [[ $(file -b "$image_file") == Linux* ]]; then
         extract_img_data "$image_file" "$image_dir"
     elif [[ $(file -b "$image_file") == Android* ]]; then
         "$SIMG2IMG" "$image_file" "$image_dir"/"$(basename "$image_file").raw"
