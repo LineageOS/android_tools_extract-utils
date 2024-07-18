015783ee87b45811feeb82df0d85eab1dfe59372
nift4 <nift4@protonmail.com>
2024-07-18 19:08:20 +0200

    extract-utils: make image extraction follow symlinks as well
    * `adb pull` always follows symlinks on device, and adb and file-based
      extraction should behave the same
    
    Change-Id: I2396f2e5b56856f5f40c1e271dd2791706672b19

--- extract_utils.sh
+++ extract_utils.sh
@@ -1497,12 +1497,12 @@ function get_file() {
         return 1
     else
         # try to copy
-        cp -r "$SRC/$1"           "$2" 2>/dev/null && return 0
-        cp -r "$SRC/${1#/system}" "$2" 2>/dev/null && return 0
-        cp -r "$SRC/system/$1"    "$2" 2>/dev/null && return 0
+        cp -Lr "$SRC/$1"           "$2" 2>/dev/null && return 0
+        cp -Lr "$SRC/${1#/system}" "$2" 2>/dev/null && return 0
+        cp -Lr "$SRC/system/$1"    "$2" 2>/dev/null && return 0
 
         # try /vendor/odm for devices without /odm partition
-        [[ "$1" == /system/odm/* ]] && cp -r "$SRC/vendor/${1#/system}" "$2" 2>/dev/null && return 0
+        [[ "$1" == /system/odm/* ]] && cp -Lr "$SRC/vendor/${1#/system}" "$2" 2>/dev/null && return 0
 
         return 1
     fi
