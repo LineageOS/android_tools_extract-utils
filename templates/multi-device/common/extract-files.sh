8c281b56ad1795a4111362b35e008db3a827f616
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-26 01:12:29 +0300

    extract-utils: allow sha1sum optimization
    Add a new blob_fixup_dry function which can be overriden
    and which will return 1 if the blob is not handled,
    and 0 if it is handled. If the blob is not handled then
    we can skip an sha1sum call.
    The default implementation returns 0 for all blobs.
    
    Change-Id: I95e15702f06897999ed7c3989d7414d17374fc6e

--- templates/multi-device/common/extract-files.sh
+++ templates/multi-device/common/extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -65,12 +64,22 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib/libsample5.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
             ;;
         vendor/lib/libsample7.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --set-soname "libsample7.so" "${2}"
             ;;
+        *)
+            return 1
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 function prepare_firmware() {
