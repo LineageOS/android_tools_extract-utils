16ff9473f03962a1c0f9798bc8da962bc730dc9a
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 21:30:32 +0300

    templates: Add vendor_imports example
    Change-Id: I40ba8b0016c35bd63e234be8a888d9692cdf894a

--- templates/multi-device/device/setup-makefiles.sh
+++ templates/multi-device/device/setup-makefiles.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/foo/baz",
+EOF
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
