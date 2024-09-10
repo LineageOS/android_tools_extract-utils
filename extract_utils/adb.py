dc38f0982d4197c876a779040807fc066386e45b
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/adb.py
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+from subprocess import SubprocessError
+from time import sleep
+
+from extract_utils.utils import run_cmd
+
+
+def adb_connected():
+    output = None
+    try:
+        output = run_cmd(['adb', 'get-state'])
+    except SubprocessError:
+        pass
+    return output == 'device\n'
+
+
+def init_adb_connection():
+    run_cmd(['adb', 'start-server'])
+    if not adb_connected():
+        print('No device is online. Waiting for one...')
+        print('Please connect USB and/or enable USB debugging')
+        while not adb_connected():
+            sleep(1)
+
+    # TODO: TCP connection
+
+    run_cmd(['adb', 'root'])
+    run_cmd(['adb', 'wait-for-device'])
