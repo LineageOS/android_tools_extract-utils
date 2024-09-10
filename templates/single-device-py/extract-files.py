4d784a9c353568ae9a9dfab7224fac4e4fc61ca3
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ templates/single-device-py/extract-files.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+from extract_utils.fixups_blob import (
+    blob_fixups_user_type,
+    blob_fixup,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixups_user_type,
+    lib_fixup_vendorcompat,
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+
+namespace_imports = [
+    'device/lineage/example',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    (
+        'vendor.twopac.hardware.xoo@1.0',
+        'vendor.twopac.hardware.oxo@1.0',
+        'vendor.twopac.hardware.oox@1.0',
+    ): lib_fixup_vendor_suffix,
+    'libwpa_client': lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/app/Test.apk': blob_fixup()
+        .apktool_patch('blob-patches/TestApk.patch', '-s'),
+    'vendor/bin/test': blob_fixup()
+        .fix_soname()
+        .add_needed('to_add.so')
+        .remove_needed('to_remove.so')
+        .replace_needed('from.so', 'to.so')
+        .binary_regex_replace(b'\xFF\x00\x00\x94', b'\xFE\x00\x00\x94')
+        .sig_replace('C0 03 5F D6 ?? ?? ?? ?? C0 03 5F D6', '1F 20 03 D5'),
+    'vendor/etc/test.conf': blob_fixup()
+        .patch('blob-patches/TestConf.patch')
+        .regex_replace('(LOG_.*_ENABLED)=1', '\\1=0')
+        .add_line_if_missing('DEBUG=0'),
+    'vendor/etc/test.xml': blob_fixup()
+        .fix_xml()
+}
+
+module = ExtractUtilsModule(
+    'example',
+    'lineage',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
