712bfb4fd6317152571e51516292b330b91530fd
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/fixups.py
@@ -0,0 +1,380 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os
+import re
+import shutil
+import tempfile
+
+from os import path
+from typing import Callable, Dict, List, Optional, Protocol, Tuple, TypeVar
+from subprocess import run
+
+from extract_utils.elf import file_needs_lib
+
+from .file import File
+from .tools import \
+    get_apktool_path, \
+    get_java_path, \
+    get_patchelf_path, \
+    get_stripzip_path
+
+
+class BlobFixupCtx:
+    def __init__(self, module_dir: str):
+        self.module_dir = module_dir
+
+
+class blob_fixup_fn_impl_type(Protocol):
+    def __call__(self, ctx: BlobFixupCtx, file: File,
+                 file_path: str,  *args,
+                 tmp_dir: str | None = None, ** kwargs):
+        ...
+
+
+class blob_fixup:
+    def __init__(self):
+        self.__patchelf_path = get_patchelf_path()
+        self.__functions: List[blob_fixup_fn_impl_type] = []
+        self.__create_tmp_dir = False
+
+    def run_cmd(self, parts: List[str]):
+        run(parts, check=True)
+
+    def call(self, fn: blob_fixup_fn_impl_type, need_tmp_dir=True):
+        self.__functions.append(fn)
+        if need_tmp_dir:
+            self.__create_tmp_dir = True
+        return self
+
+    def patchelf_version(self, version: str):
+        self.__patchelf_path = get_patchelf_path(version)
+
+    def replace_needed_impl(self, from_lib: str, to_lib: str,
+                            ctx: BlobFixupCtx, file: File,
+                            file_path: str, *args, **kargs):
+        self.run_cmd([
+            self.__patchelf_path,
+            '--replace-needed',
+            from_lib, to_lib,
+            file_path])
+
+    def replace_needed(self, from_lib: str, to_lib: str):
+        def impl(*args, **kwargs):
+            self.replace_needed_impl(from_lib, to_lib, *args, *kwargs)
+
+        return self.call(impl)
+
+    def add_needed_impl(self, lib: str,
+                        ctx: BlobFixupCtx, file: File,
+                        file_path: str, *args, **kargs):
+        if file_needs_lib(file_path, lib):
+            return
+
+        self.run_cmd([
+            self.__patchelf_path,
+            '--add-needed',
+            lib,
+            file_path])
+
+    def add_needed(self, lib: str):
+        def impl(*args, **kwargs):
+            self.add_needed_impl(lib, *args, **kwargs)
+
+        return self.call(impl)
+
+    def remove_needed_impl(self, lib: str,
+                           ctx: BlobFixupCtx, file: File,
+                           file_path: str, *args, **kargs):
+        self.run_cmd([
+            self.__patchelf_path,
+            '--remove-needed',
+            lib,
+            file_path])
+
+    def remove_needed(self, lib: str):
+        def impl(*args, **kwargs):
+            self.remove_needed_impl(lib, *args, **kwargs)
+
+        return self.call(impl)
+
+    def fix_soname_impl(self,
+                        ctx: BlobFixupCtx, file: File,
+                        file_path: str, *args, **kargs):
+        self.run_cmd([
+            self.__patchelf_path,
+            '--set-soname',
+            file.basename,
+            file_path])
+
+    def fix_soname(self):
+        return self.call(self.fix_soname_impl)
+
+    def __get_patches(self, ctx: BlobFixupCtx, module_patches_path: str):
+        patches_path = path.join(ctx.module_dir, module_patches_path)
+
+        if path.isfile(patches_path):
+            return [patches_path]
+
+        assert path.isdir(patches_path)
+
+        patches = []
+        for f in os.scandir(patches_path):
+            if f.name.endswith('.patch'):
+                patches.append(f.path)
+
+        patches.sort()
+
+        return patches
+
+    def patch_impl(self, patches_path: str,
+                   ctx: BlobFixupCtx, file: File,
+                   file_path: str, *args,
+                   tmp_dir=None, **kargs):
+        patches = self.__get_patches(ctx, patches_path)
+        assert tmp_dir is not None
+        self.run_cmd([
+            'git',
+            'apply',
+            '--unsafe-path',
+            '--directory', tmp_dir,
+            *patches
+        ])
+
+    def patch_dir(self, patches_path: str):
+        def impl(*args, **kwargs):
+            self.patch_impl(patches_path, *args, **kwargs)
+
+        return self.call(impl, need_tmp_dir=True)
+
+    def copy_file_to_tmp_impl(self, ctx: BlobFixupCtx, file: File,
+                              file_path: str, *args,
+                              tmp_dir=None, **kargs):
+        assert tmp_dir is not None
+        shutil.copy(file_path, tmp_dir)
+
+    def copy_file_to_tmp(self):
+        return self.call(self.copy_file_to_tmp_impl, need_tmp_dir=True)
+
+    def copy_file_from_tmp_impl(self, ctx: BlobFixupCtx, file: File,
+                                file_path: str, *args,
+                                tmp_dir=None, **kargs):
+        assert tmp_dir is not None
+        tmp_file_path = path.join(tmp_dir, file.basename)
+        shutil.copy(tmp_file_path, file_path)
+
+    def copy_file_from_tmp(self):
+        return self.call(self.copy_file_from_tmp_impl, need_tmp_dir=True)
+
+    def patch_file(self, patches_path: str):
+        self.copy_file_to_tmp()
+        self.patch_dir(patches_path)
+        self.copy_file_from_tmp()
+        return self
+
+    def apktool_unpack_impl(self, ctx: BlobFixupCtx, file: File,
+                            file_path: str, *args,
+                            tmp_dir=None, **kargs):
+        java_path = get_java_path()
+        apktool_path = get_apktool_path()
+
+        assert tmp_dir is not None
+
+        self.run_cmd([
+            java_path,
+            '-jar', apktool_path,
+            'd', file_path,
+            '-o', tmp_dir,
+            '-f',
+        ])
+
+    def apktool_unpack(self):
+        return self.call(self.apktool_unpack_impl, need_tmp_dir=True)
+
+    def apktool_pack_impl(self, ctx: BlobFixupCtx, file: File,
+                          file_path: str, *args,
+                          tmp_dir=None, **kargs):
+        java_path = get_java_path()
+        apktool_path = get_apktool_path()
+        assert tmp_dir is not None
+
+        self.run_cmd([
+            java_path,
+            '-jar', apktool_path,
+            'b', tmp_dir,
+            '-o', file_path,
+        ])
+
+    def apktool_pack(self):
+        return self.call(self.apktool_pack_impl, need_tmp_dir=True)
+
+    def stripzip_impl(self, ctx: BlobFixupCtx, file: File,
+                      file_path: str, *args, **kargs):
+        stripzip_path = get_stripzip_path()
+        self.run_cmd([
+            stripzip_path,
+            file_path,
+        ])
+
+    def stripzip(self):
+        return self.call(self.stripzip_impl)
+
+    def apktool_patch(self, patches_path: str):
+        self.apktool_unpack()
+        self.patch_dir(patches_path)
+        self.apktool_pack()
+        self.stripzip()
+        return self
+
+    def regex_replace_impl(self, text: str, replacement: str,
+                           ctx: BlobFixupCtx, file: File,
+                           file_path: str, *args, **kargs):
+        with open(file_path, 'r') as f:
+            data = f.read()
+
+        data = re.sub(text, replacement, data)
+
+        with open(file_path, 'w') as f:
+            f.write(data)
+
+    def regex_replace(self, search: str, replace: str):
+        def impl(*args, **kwargs):
+            self.regex_replace_impl(search, replace, *args, **kwargs)
+
+        return self.call(impl)
+
+    def run(self, ctx: BlobFixupCtx, file: File, file_path: str) -> bool:
+        def run(tmp_dir: str | None = None):
+            for function in self.__functions:
+                function(ctx, file, file_path, tmp_dir=tmp_dir)
+
+        if self.__create_tmp_dir:
+            with tempfile.TemporaryDirectory() as tmp_dir:
+                run(tmp_dir)
+        else:
+            run()
+
+        return True
+
+
+T = TypeVar('T')
+
+fixups_user_type = Dict[str | Tuple[str, ...], T]
+fixups_type = Dict[str, T]
+
+blob_fixup_fn_type = blob_fixup
+blob_fixups_user_type = fixups_user_type[blob_fixup_fn_type]
+blob_fixups_type = fixups_type[blob_fixup_fn_type]
+
+lib_fixup_fn_type = Callable[[str, str], Optional[str]]
+lib_fixups_user_type = fixups_user_type[lib_fixup_fn_type]
+lib_fixups_type = fixups_type[lib_fixup_fn_type]
+
+
+def flatten_fixups(fixups: Optional[fixups_user_type[T]]) -> Optional[fixups_type[T]]:
+    if fixups is None:
+        return None
+
+    fixups_final: fixups_type = {}
+
+    for entries, value in fixups.items():
+        if isinstance(entries, str):
+            fixups_final[entries] = value
+        elif isinstance(entries, tuple):
+            for entry in entries:
+                fixups_final[entry] = value
+        else:
+            assert False
+
+    return fixups_final
+
+
+libs_clang_rt_ubsan = (
+    'libclang_rt.ubsan_standalone-arm-android',
+    'libclang_rt.ubsan_standalone-aarch64-android'
+)
+
+
+libs_proto_3_9_1 = (
+    'libprotobuf-cpp-lite-3.9.1',
+    'libprotobuf-cpp-full-3.9.1'
+)
+
+
+def lib_fixup_remove_arch_suffix(lib: str, *args, **kwargs):
+    suffixes = ['-arm-android', '-aarch64-android']
+    for suffix in suffixes:
+        removed = lib.removesuffix(suffix)
+        if removed != lib:
+            return removed
+
+    assert False
+
+
+def lib_fixup_vendorcompat(lib: str, *args, **kwargs):
+    return f'{lib}-vendorcompat'
+
+
+lib_fixups = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+
+def run_lib_fixup(fixups: lib_fixups_type | None,
+                  lib: str, partition: str) -> str:
+    if fixups is None:
+        return lib
+
+    lib_fixup_fn = fixups.get(lib)
+    if lib_fixup_fn is None:
+        return lib
+
+    fixed_up_lib = lib_fixup_fn(lib, partition)
+    if fixed_up_lib is None:
+        return lib
+
+    return fixed_up_lib
+
+
+def run_libs_fixup(fixups: lib_fixups_type | None,
+                   libs: List[str] | None, partition: str):
+    if libs is None:
+        return None
+
+    if fixups is None:
+        return libs
+
+    fixed_libs = []
+    for lib in libs:
+        fixed_lib = run_lib_fixup(fixups, lib, partition)
+        if fixed_lib == '':
+            continue
+
+        fixed_libs.append(lib)
+
+    return fixed_libs
+
+
+def run_blob_fixup(fixups: Optional[blob_fixups_type],
+                   ctx: Optional[BlobFixupCtx], file: File,
+                   file_path: Optional[str]) -> bool:
+    if fixups is None:
+        return False
+
+    blob_fixup_fn = fixups.get(file.dst)
+    if blob_fixup_fn is None:
+        return False
+
+    if file_path is None:
+        return True
+
+    assert isinstance(blob_fixup_fn, blob_fixup)
+    assert ctx is not None
+
+    blob_fixup_fn.run(ctx, file, file_path)
+
+    return True
---
+++ extract_utils/makefiles.py
@@ -0,0 +1,376 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from typing import Callable, List, Optional, TextIO
+
+from .bp_builder import BpBuilder, ModuleClass
+from .elf import get_file_arch_bits, get_file_arch_bits_libs
+from .file import CommonFileTree, File, FileArgs, FileTree
+from .fixups import lib_fixups_type, run_libs_fixup
+
+# TODO: find out if partition-less files are a thing
+ALL_PARTITIONS = ['system', 'vendor', 'product', 'system_ext', 'odm']
+APEX_PARTITIONS = ['system', 'vendor', 'system_ext']
+RFSA_PARTITIONS = ['vendor', 'odm']
+
+
+class MakefilesCtx:
+    def __init__(self, device: str, vendor: str,
+                 vendor_files_path: str,
+                 vendor_files_rel_path: str,
+                 vendor_files_rel_sub_path: str,
+                 vendor_imports: Optional[List[str]],
+                 lib_fixups: Optional[lib_fixups_type],
+                 target_enable_checkelf: bool):
+        self.device = device
+        self.vendor = vendor
+        self.files_rel_path = vendor_files_rel_path
+        self.files_path = vendor_files_path
+        self.rel_sub_path = vendor_files_rel_sub_path
+
+        self.vendor_imports = vendor_imports
+        self.lib_fixups = lib_fixups
+        self.target_enable_checkelf = target_enable_checkelf
+
+    def path(self, file: File):
+        return f'{self.files_path}/{file.dst}'
+
+
+def file_gen_deps_check_elf(ctx: MakefilesCtx, file: File):
+    gen_deps = False
+    enable_checkelf = False
+
+    if ctx.target_enable_checkelf:
+        gen_deps = True
+        enable_checkelf = True
+
+    if FileArgs.DISABLE_CHECKELF in file.args:
+        enable_checkelf = False
+
+    if FileArgs.DISABLE_DEPS in file.args:
+        gen_deps = False
+        enable_checkelf = False
+
+    return gen_deps, enable_checkelf
+
+
+def write_sh_package(ctx: MakefilesCtx, builder: BpBuilder):
+    return builder \
+        .cls(ModuleClass.SH_BINARIES) \
+        .name() \
+        .stem() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .filename() \
+        .specific()
+
+
+def write_bin_package(ctx: MakefilesCtx, builder: BpBuilder):
+    file = builder.get_file()
+    partition = builder.get_partition()
+
+    if file.ext == '.sh':
+        return write_sh_package(ctx, builder)
+
+    gen_deps, enable_checkelf = file_gen_deps_check_elf(ctx, file)
+    file_path = ctx.path(file)
+    arch, bits, deps = get_file_arch_bits_libs(file_path, gen_deps)
+
+    if arch is None or bits is None:
+        return write_sh_package(ctx, builder)
+
+    deps = run_libs_fixup(ctx.lib_fixups, deps, partition)
+
+    builder \
+        .cls(ModuleClass.EXECUTABLES) \
+        .name() \
+        .stem() \
+        .owner(ctx.vendor) \
+        .target(ctx.rel_sub_path, file, arch, deps) \
+        .multilib(bits) \
+        .check_elf(enable_checkelf) \
+        .no_strip() \
+        .prefer() \
+        .relative_install_path() \
+        .specific()
+
+
+def write_libs_package(ctx: MakefilesCtx, builder: BpBuilder):
+    file = builder.get_file()
+    files = builder.get_files()
+    partition = builder.get_partition()
+
+    gen_deps, enable_check_elf = file_gen_deps_check_elf(ctx, file)
+    file_path = ctx.path(file)
+    arch, bits, deps = get_file_arch_bits_libs(file_path, gen_deps)
+    assert arch is not None
+    assert bits is not None
+    arches = [arch]
+    bitses = [bits]
+
+    deps = run_libs_fixup(ctx.lib_fixups, deps, partition)
+
+    for f in files[1:]:
+        f_path = ctx.path(f)
+        arch, bits = get_file_arch_bits(f_path)
+        assert arch is not None
+        assert bits is not None
+        arches.append(arch)
+        bitses.append(bits)
+
+    builder \
+        .cls(ModuleClass.SHARED_LIBRARIES) \
+        .name() \
+        .stem() \
+        .owner(ctx.vendor) \
+        .no_strip() \
+        .targets(ctx.rel_sub_path, files, arches, deps) \
+        .multilibs(bitses) \
+        .check_elf(enable_check_elf) \
+        .relative_install_path() \
+        .prefer() \
+        .specific()
+
+
+def write_rfsa_package(ctx: MakefilesCtx, builder: BpBuilder):
+    builder \
+        .cls(ModuleClass.RFSA) \
+        .name() \
+        .filename() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .relative_install_path() \
+        .specific()
+
+
+def write_apex_package(ctx: MakefilesCtx, builder: BpBuilder):
+    builder \
+        .cls(ModuleClass.APEX) \
+        .name() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .filename() \
+        .specific()
+
+
+def write_app_package(ctx: MakefilesCtx, builder: BpBuilder):
+    file = builder.get_file()
+
+    # TODO: remove required entries from package_names if actually needed
+    # TODO: check if manually specified certificates are needed
+    builder \
+        .cls(ModuleClass.APPS) \
+        .name() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .set('overrides', file.overrides, optional=True) \
+        .set('required', file.required, optional=True) \
+        .signature() \
+        .set('dex_preopt', {
+            'enabled': False
+        }) \
+        .set('privileged', file.privileged, optional=True) \
+        .specific()
+
+
+def write_framework_package(ctx: MakefilesCtx, builder: BpBuilder):
+    builder \
+        .cls(ModuleClass.JAVA_LIBRARIES) \
+        .name() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .specific()
+
+
+def write_etc_package(ctx: MakefilesCtx, builder: BpBuilder):
+    file = builder.get_file()
+
+    if file.ext == '.xml':
+        cls = ModuleClass.ETC_XML
+    else:
+        cls = ModuleClass.ETC
+
+    builder \
+        .cls(cls) \
+        .name() \
+        .owner(ctx.vendor) \
+        .src(ctx.rel_sub_path) \
+        .set('filename_from_src', True) \
+        .sub_dir() \
+        .specific()
+
+
+def write_packages_group(ctx: MakefilesCtx, fn: Callable[[MakefilesCtx, BpBuilder], None],
+                         file_tree: FileTree, package_names: List[str], out: TextIO):
+    # If the file tree is a common one, the contents might be lists of files
+    file_is_list = isinstance(file_tree, CommonFileTree)
+
+    for file in file_tree:
+        builder = BpBuilder() \
+            .prefix_len(file_tree.parts_prefix_len) \
+            .partition(file_tree.parts[0])
+
+        if file_is_list:
+            assert isinstance(file, list)
+            builder.files(file)
+        else:
+            builder.file(file)
+
+        fn(ctx, builder)
+
+        builder.write(out)
+
+        package_name = builder.get_package_name()
+        package_names.append(package_name)
+
+
+def write_packages_inclusion(out, package_names):
+    if not package_names:
+        return
+
+    out.write('\n')
+    out.write('PRODUCT_PACKAGES +=')
+    for package_name in package_names:
+        line = f' \\\n    {package_name}'
+        out.write(line)
+    out.write('\n')
+
+
+def write_product_packages(ctx: MakefilesCtx, base_file_tree: FileTree,
+                           bp_out: TextIO, mk_out: TextIO):
+    package_names = []
+
+    def w(fn, file_tree):
+        return write_packages_group(ctx, fn, file_tree, package_names, bp_out)
+
+    def wp(fn, part, sub_dir):
+        file_tree = base_file_tree.filter_prefixed([part, sub_dir])
+
+        return w(fn, file_tree)
+
+    # Extract these first so that they don't end up in lib32
+    for part in RFSA_PARTITIONS:
+        parts = [part, 'lib', 'rfsa']
+        lib_rfsa_tree = base_file_tree.filter_prefixed(parts)
+        w(write_rfsa_package, lib_rfsa_tree)
+
+    for part in ALL_PARTITIONS:
+        parts = [part]
+
+        lib32_tree = base_file_tree.filter_prefixed(parts + ['lib'])
+        lib64_tree = base_file_tree.filter_prefixed(parts + ['lib64'])
+        lib_common_tree = CommonFileTree.common_files(lib32_tree, lib64_tree)
+
+        fn = write_libs_package
+        w(fn, lib_common_tree)
+        w(fn, lib32_tree)
+        w(fn, lib64_tree)
+
+    for part in APEX_PARTITIONS:
+        wp(write_apex_package, part, 'apex')
+
+    for part in ALL_PARTITIONS:
+        for sub_dir in ['app', 'priv-app']:
+            wp(write_app_package, part, sub_dir)
+
+    for part in ALL_PARTITIONS:
+        wp(write_framework_package, part, 'framework')
+
+    for part in ALL_PARTITIONS:
+        wp(write_etc_package, part, 'etc')
+
+    for part in ALL_PARTITIONS:
+        wp(write_bin_package, part, 'bin')
+
+    assert not list(base_file_tree)
+
+    write_packages_inclusion(mk_out, package_names)
+
+
+def write_product_copy_files(ctx: MakefilesCtx, file_tree: FileTree,
+                             out: TextIO):
+    if not file_tree:
+        return
+
+    out.write('PRODUCT_COPY_FILES +=')
+
+    for file in file_tree:
+        partition = file.parts[0]
+        target = f'$(TARGET_COPY_OUT_{partition.upper()})'
+        rel_dst = file.dst[len(partition):]
+        line = f' \\\n    {ctx.files_rel_path}/{file.dst}:{target}{rel_dst}'
+
+        out.write(line)
+
+    out.write('\n')
+
+
+def write_symlink_package(file: File, symlink: str,
+                          package_names: List[str], out: TextIO):
+    symlink_target = f'/{file.dst}'
+    # TODO: symlinks outside of partitions?
+    part, location = symlink.split('/', 1)
+    package_name = symlink.replace('/', '_')
+
+    BpBuilder() \
+        .cls(ModuleClass.SYMLINK) \
+        .partition(part) \
+        .raw_name(package_name) \
+        .specific() \
+        .set('installed_location', location) \
+        .set('symlink_target', symlink_target) \
+        .write(out)
+
+    package_names.append(package_name)
+
+
+def write_symlink_packages(ctx: MakefilesCtx, file_tree: FileTree,
+                           bp_out: TextIO, mk_out: TextIO):
+    package_names = []
+
+    for file in file_tree:
+        for symlink in file.symlinks:
+            write_symlink_package(file, symlink, package_names, bp_out)
+
+    write_packages_inclusion(mk_out, package_names)
+
+
+def write_mk_header(ctx: MakefilesCtx, out: TextIO):
+    out.write(
+        f'''
+# Automatically generated file. DO NOT MODIFY
+#
+# This file is generated by device/{ctx.vendor}/{ctx.device}/extract-files.py
+
+'''.lstrip()
+    )
+
+
+def write_bp_header(ctx: MakefilesCtx, out: TextIO):
+    out.write(
+        f'''
+// Automatically generated file. DO NOT MODIFY
+//
+// This file is generated by device/{ctx.vendor}/{ctx.device}/extract-files.py
+
+'''.lstrip()
+    )
+
+
+def write_mk_soong_namespaces(ctx: MakefilesCtx, out: TextIO):
+    out.write(
+        f'''
+PRODUCT_SOONG_NAMESPACES += \\
+    vendor/{ctx.vendor}/{ctx.device}
+'''.lstrip()
+    )
+
+
+def write_bp_soong_namespaces(ctx: MakefilesCtx, out: TextIO):
+    BpBuilder() \
+        .rule_name('soong_namespace') \
+        .set('imports', ctx.vendor_imports) \
+        .write(out)
