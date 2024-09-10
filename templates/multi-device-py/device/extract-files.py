7137b2851a609858de6d2cd7a1730efcd8980b66
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ templates/multi-device-py/device/extract-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/lineage/example',
+]
+
+module = ExtractUtilsModule(
+    'example_plus',
+    'lineage',
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'example', module.vendor
+    )
+    utils.run()
