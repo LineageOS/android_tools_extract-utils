83e2f1ed13e27b8e003e132d41cf522427e7880b
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/extract.py
@@ -0,0 +1,503 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+from contextlib import contextmanager
+import os
+import shutil
+import tempfile
+
+from os import path
+from typing import Callable, List, Optional
+
+
+from extract_utils.args import ArgsSource
+from extract_utils.utils import (
+    flatten_dir,
+    parallel_input_cmds,
+    process_cmds_in_parallel,
+)
+from extract_utils.tools import (
+    get_brotli_path,
+    get_lpunpack_path,
+    get_ota_extractor_path,
+    get_sdat2img_path,
+    get_simg2img_path,
+)
+
+DEFAULT_EXTRACTED_PARTITIONS = [
+    'system',
+    'odm',
+    'product',
+    'system_ext',
+    'vendor',
+]
+BROTLI_EXT = '.new.dat.br'
+SPARSE_DATA_EXT = '.new.dat'
+TRANSFER_LIST_EXT = '.transfer.list'
+SPARSE_CHUNK_SUFFIX = '_sparsechunk'
+
+
+extract_fn_type = Callable[['ExtractCtx', str, str], List[str]]
+
+
+class ExtractCtx:
+    def __init__(
+        self,
+        source: str | ArgsSource,
+        keep_dump: bool,
+        extract_fns: List[extract_fn_type],
+        extract_partitions: Optional[List[str]],
+    ):
+        self.source = source
+        self.keep_dump = keep_dump
+        self.extract_fns = extract_fns
+
+        if not extract_partitions:
+            extract_partitions = DEFAULT_EXTRACTED_PARTITIONS
+        self.extract_partitions = extract_partitions
+
+
+def is_extract_partition_file_name(
+    extract_partitions: Optional[List[str]],
+    file_name: str,
+):
+    if extract_partitions is None:
+        return True
+
+    root, _ = file_name.split('.', 1)
+    return root in extract_partitions
+
+
+def find_files_with_magic(
+    extract_partitions: Optional[List[str]],
+    input_path: str,
+    magic: bytes,
+    position: int = 0,
+) -> List[str]:
+    file_paths = []
+    for file in os.scandir(input_path):
+        if not path.isfile(file):
+            continue
+
+        if not is_extract_partition_file_name(extract_partitions, file.name):
+            continue
+
+        with open(file.path, 'rb') as f:
+            f.seek(position)
+            file_magic = f.read(len(magic))
+            if file_magic == magic:
+                file_paths.append(file.path)
+
+    return file_paths
+
+
+def find_files_with_ext(
+    extract_partitions: Optional[List[str]],
+    input_path: str,
+    ext: str,
+):
+    file_paths = []
+    for file in os.scandir(input_path):
+        if not path.isfile(file):
+            continue
+
+        if not is_extract_partition_file_name(extract_partitions, file.name):
+            continue
+
+        if file.name.endswith(ext):
+            file_paths.append(file.path)
+
+    return file_paths
+
+
+def find_sparse_raw_image_paths(
+    extract_partitions: Optional[List[str]],
+    input_path: str,
+):
+    magic = bytes([0x3A, 0xFF, 0x26, 0xED])
+    return find_files_with_magic(extract_partitions, input_path, magic)
+
+
+def find_erofs_paths(
+    extract_partitions: Optional[List[str]],
+    input_path: str,
+):
+    magic = bytes([0xE2, 0xE1, 0xF5, 0xE0])
+    return find_files_with_magic(extract_partitions, input_path, magic, 1024)
+
+
+def find_ext4_paths(
+    extract_partitions: Optional[List[str]],
+    input_path: str,
+):
+    magic = bytes([0x53, 0xEF])
+    return find_files_with_magic(extract_partitions, input_path, magic, 1080)
+
+
+def find_payload_path(input_path: str) -> Optional[str]:
+    payload_paths = find_files_with_magic(None, input_path, b'CrAU')
+    if payload_paths:
+        assert len(payload_paths) == 1
+        return payload_paths[0]
+
+    return None
+
+
+def find_super_img_path(input_path: str) -> Optional[str]:
+    super_img_path = path.join(input_path, 'super.img')
+    if path.isfile(super_img_path):
+        return super_img_path
+
+    return None
+
+
+def print_file_paths(file_paths: List[str], file_type: str):
+    file_names = [path.basename(fp) for fp in file_paths]
+    file_names_str = ', '.join(file_names)
+    print(f'Found {file_type} files: {file_names_str}')
+
+
+def remove_file_paths(file_paths: List[str]):
+    file_names = [path.basename(fp) for fp in file_paths]
+    file_names_str = ', '.join(file_names)
+    print(f'Deleting {file_names_str}')
+
+    for file_path in file_paths:
+        os.remove(file_path)
+
+
+def extract_payload_bin(ctx: ExtractCtx, file_path: str, output_dir: str):
+    ota_extractor_path = get_ota_extractor_path()
+
+    procs: parallel_input_cmds = []
+    for partition in ctx.extract_partitions:
+        procs.append(
+            (
+                partition,
+                [
+                    ota_extractor_path,
+                    '--payload',
+                    file_path,
+                    '--output-dir',
+                    output_dir,
+                    '--partitions',
+                    partition,
+                ],
+            )
+        )
+
+    process_cmds_in_parallel(procs)
+
+
+def extract_sparse_raw_imgs(file_paths: List[str], output_dir: str):
+    new_file_paths = []
+    simg2img_path = get_simg2img_path()
+
+    partition_chunks_map = {}
+    for file_path in file_paths:
+        file_name = path.basename(file_path)
+
+        # Split extension to get chunk index x from
+        # partition.img_sparsechunk.x files
+        base_file_name, chunk_index = path.splitext(file_name)
+
+        if base_file_name.endswith(SPARSE_CHUNK_SUFFIX) and chunk_index:
+            # Sparse chunk, remove the suffix to get the partition name
+            output_file_name = base_file_name[: -len(SPARSE_CHUNK_SUFFIX)]
+            # Remove dot from extension and cast to int find chunk index
+            chunk_index = int(chunk_index[1:])
+        else:
+            output_file_name = file_name
+            chunk_index = 0
+
+            # Rename single sparse image to .sparse to avoid naming conflicts
+            sparse_file_path = f'{file_path}.sparse'
+            os.rename(file_path, sparse_file_path)
+            file_path = sparse_file_path
+
+        new_file_paths.append(file_path)
+
+        # Create a sparse list of the chunks, should be completely filled
+        # after iterating over all the file paths
+        # Do this to avoid splitting the file paths again to sort at the end
+        partition_chunks_map.setdefault(output_file_name, [])
+        partition_chunks = partition_chunks_map[output_file_name]
+        assert isinstance(partition_chunks, list)
+
+        missing_indices = chunk_index - len(partition_chunks) + 1
+        partition_chunks.extend([None] * missing_indices)
+        assert partition_chunks[chunk_index] is None
+
+        partition_chunks[chunk_index] = file_path
+
+    procs: parallel_input_cmds = []
+    for output_file_name, partition_chunks in partition_chunks_map.items():
+        output_file_path = path.join(output_dir, output_file_name)
+
+        procs.append(
+            (
+                output_file_name,
+                [simg2img_path] + partition_chunks + [output_file_path],
+            )
+        )
+
+    process_cmds_in_parallel(procs, fatal=True)
+
+    return new_file_paths
+
+
+def extract_super_img(ctx: ExtractCtx, file_path: str, output_dir: str):
+    lpunpack_path = get_lpunpack_path()
+
+    procs: parallel_input_cmds = []
+    for partition in ctx.extract_partitions:
+        for slot in ['', '_a']:
+            partition_slot = f'{partition}{slot}'
+            procs.append(
+                (
+                    partition_slot,
+                    [
+                        lpunpack_path,
+                        '--partition',
+                        partition_slot,
+                        file_path,
+                        output_dir,
+                    ],
+                )
+            )
+
+    process_cmds_in_parallel(procs)
+
+    for partition in ctx.extract_partitions:
+        partition_a_img = f'{partition}_a.img'
+        partition_img = f'{partition}.img'
+
+        partition_a_path = path.join(output_dir, partition_a_img)
+        partition_path = path.join(output_dir, partition_img)
+
+        if path.exists(partition_a_path):
+            os.rename(partition_a_path, partition_path)
+
+
+def extract_brotli_imgs(file_paths: List[str], output_path: str):
+    brotli_path = get_brotli_path()
+
+    procs: parallel_input_cmds = []
+    for file_path in file_paths:
+        file_name = path.basename(file_path)
+        output_file_name, _ = path.splitext(file_name)
+        output_file_path = path.join(output_path, output_file_name)
+
+        procs.append(
+            (file_name, [brotli_path, '-d', file_path, '-o', output_file_path])
+        )
+
+    process_cmds_in_parallel(procs, fatal=True)
+
+
+def extract_sparse_data_imgs(file_paths: List[str], output_path: str):
+    sdat2img_path = get_sdat2img_path()
+
+    procs: parallel_input_cmds = []
+    for file_path in file_paths:
+        assert file_path.endswith(SPARSE_DATA_EXT)
+
+        base_file_path = file_path[: -len(SPARSE_DATA_EXT)]
+        transfer_file_path = f'{base_file_path}{TRANSFER_LIST_EXT}'
+
+        base_file_name = path.basename(base_file_path)
+        img_file_name = f'{base_file_name}.img'
+
+        output_file_path = path.join(output_path, img_file_name)
+
+        procs.append(
+            (
+                base_file_name,
+                [
+                    sdat2img_path,
+                    transfer_file_path,
+                    file_path,
+                    output_file_path,
+                ],
+            )
+        )
+
+    process_cmds_in_parallel(procs, fatal=True)
+
+
+def extract_erofs(file_paths: List[str], output_path: str):
+    procs: parallel_input_cmds = []
+    for file_path in file_paths:
+        base_file_name = path.basename(file_path)
+
+        partition_name, _ = path.splitext(base_file_name)
+        partition_output_path = path.join(output_path, partition_name)
+        os.mkdir(partition_output_path)
+
+        procs.append(
+            (
+                base_file_name,
+                [
+                    'fsck.erofs',
+                    f'--extract={partition_output_path}',
+                    file_path,
+                ],
+            )
+        )
+
+    process_cmds_in_parallel(procs, fatal=True)
+
+
+def extract_ext4(file_paths: List[str], output_path: str):
+    procs: parallel_input_cmds = []
+    for file_path in file_paths:
+        base_file_name = path.basename(file_path)
+
+        partition_name, _ = path.splitext(base_file_name)
+        partition_output_path = path.join(output_path, partition_name)
+        os.mkdir(partition_output_path)
+
+        procs.append(
+            (
+                base_file_name,
+                [
+                    'debugfs',
+                    '-R',
+                    f'rdump / {partition_output_path}',
+                    file_path,
+                ],
+            )
+        )
+
+    # TODO: check for symlinks like the old code?
+
+    process_cmds_in_parallel(procs, fatal=True)
+
+
+@contextmanager
+def get_dump_dir(ctx: ExtractCtx):
+    source = ctx.source
+    assert isinstance(source, str)
+
+    if not path.isfile(source) and not path.isdir(source):
+        # TODO: use custom error class
+        raise ValueError(f'Unexpected file type at {source}')
+
+    if path.isdir(source):
+        # Source is a directory, try to extract its contents into itself
+        print(f'Extracting to source dump dir {source}')
+        yield source, True
+        return
+
+    if not ctx.keep_dump:
+        # We don't want to keep the dump, ignore previous dump output
+        # and use a temporary directory to extract
+        with tempfile.TemporaryDirectory() as dump_dir:
+            print(f'Extracting to temporary dump dir {dump_dir}')
+            yield dump_dir, True
+            return
+
+    # Remove the extension from the file and use it as a dump dir
+    dump_dir, _ = path.splitext(source)
+
+    try:
+        os.rmdir(dump_dir)
+    except Exception:
+        pass
+
+    if path.isdir(dump_dir):
+        print(f'Using existing dump dir {dump_dir}')
+        # Previous dump output exists, return it and don't extract
+        yield dump_dir, False
+        return
+
+    if path.exists(dump_dir):
+        raise ValueError(f'Unexpected file type at {dump_dir}')
+
+    print(f'Extracting to new dump dir {dump_dir}')
+    os.mkdir(dump_dir)
+    yield dump_dir, True
+
+
+def extract_image(ctx: ExtractCtx, dump_dir: str, work_dir: str):
+    source = ctx.source
+    assert isinstance(source, str)
+
+    source_is_file = path.isfile(source)
+
+    if source_is_file:
+        print(f'Extracting file {source}')
+        shutil.unpack_archive(source, extract_dir=work_dir)
+        flatten_dir(work_dir)
+
+    payload_bin_path = find_payload_path(work_dir)
+    if payload_bin_path:
+        print_file_paths([payload_bin_path], 'payload.bin')
+        extract_payload_bin(ctx, payload_bin_path, work_dir)
+        remove_file_paths([payload_bin_path])
+
+    sparse_raw_paths = find_sparse_raw_image_paths(['super'], work_dir)
+    if sparse_raw_paths:
+        print_file_paths(sparse_raw_paths, 'sparse raw')
+        # Single sparse files are renamed to .sparse to avoid naming conflicts
+        # Retrieve the updated file paths
+        sparse_raw_paths = extract_sparse_raw_imgs(sparse_raw_paths, work_dir)
+        remove_file_paths(sparse_raw_paths)
+
+    super_img_path = find_super_img_path(work_dir)
+    if super_img_path:
+        print_file_paths([super_img_path], 'super.img')
+        extract_super_img(ctx, super_img_path, work_dir)
+        remove_file_paths([super_img_path])
+
+    brotli_paths = find_files_with_ext(
+        ctx.extract_partitions,
+        work_dir,
+        BROTLI_EXT,
+    )
+    if brotli_paths:
+        print_file_paths(brotli_paths, 'brotli')
+        extract_brotli_imgs(brotli_paths, work_dir)
+        remove_file_paths(brotli_paths)
+
+    sparse_data_paths = find_files_with_ext(
+        ctx.extract_partitions,
+        work_dir,
+        SPARSE_DATA_EXT,
+    )
+    if sparse_data_paths:
+        print_file_paths(sparse_data_paths, 'sparse data')
+        extract_sparse_data_imgs(sparse_data_paths, work_dir)
+        remove_file_paths(sparse_data_paths)
+
+    erofs_paths = find_erofs_paths(
+        ctx.extract_partitions,
+        work_dir,
+    )
+    if erofs_paths:
+        print_file_paths(erofs_paths, 'EROFS')
+        extract_erofs(erofs_paths, dump_dir)
+        remove_file_paths(erofs_paths)
+
+    ext4_paths = find_ext4_paths(
+        ctx.extract_partitions,
+        work_dir,
+    )
+    if ext4_paths:
+        print_file_paths(ext4_paths, 'EXT4')
+        extract_ext4(ext4_paths, dump_dir)
+        remove_file_paths(ext4_paths)
+
+    for extract_fn in ctx.extract_fns:
+        processed_files = extract_fn(ctx, work_dir, dump_dir)
+        remove_file_paths(processed_files)
+
+    # Move leftover files to dump dir
+    for file in os.scandir(work_dir):
+        print(f'Found leftover file {file.name}')
+        shutil.move(file.path, dump_dir)
---
+++ extract_utils/source.py
@@ -0,0 +1,222 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from contextlib import contextmanager, nullcontext
+from os import path
+import os
+import re
+import shutil
+import tempfile
+from typing import List
+
+from extract_utils.adb import init_adb_connection
+from extract_utils.args import ArgsSource
+from extract_utils.file import File, FileArgs
+from extract_utils.extract import ExtractCtx, extract_image, get_dump_dir
+from extract_utils.utils import run_cmd
+
+
+class Source(ABC):
+    @abstractmethod
+    def _list_sub_path_file_rel_paths(self, source_path: str) -> List[str]: ...
+
+    @abstractmethod
+    def _source_sub_path(self, sub_path: str) -> str: ...
+
+    @abstractmethod
+    def _copy_file_from_path(
+        self,
+        file_path: str,
+        target_file_path: str,
+    ) -> bool: ...
+
+    @abstractmethod
+    def copy_firmware(self, file: File, target_dir: str) -> bool: ...
+
+    def __copy_file_from_rel_path(
+        self,
+        file_rel_path: str,
+        target_file_path: str,
+    ) -> bool:
+        source_path = self._source_sub_path(file_rel_path)
+        file_path = path.join(source_path, file_rel_path)
+        return self._copy_file_from_path(file_path, target_file_path)
+
+    def copy_file(self, file: File, target_dir: str) -> bool:
+        target_dir_path = path.join(target_dir, file.dirname)
+        if not path.exists(target_dir_path):
+            os.makedirs(target_dir_path)
+
+        target_file_path = path.join(target_dir, file.dst)
+
+        # TODO: try source before destination by default or allow changing
+
+        if self.__copy_file_from_rel_path(file.dst, target_file_path):
+            return True
+
+        # dst is different from src, try src too
+        if file.has_dst:
+            if self.__copy_file_from_rel_path(file.src, target_file_path):
+                return True
+
+        return False
+
+    def find_sub_dir_files(
+        self,
+        sub_path: str,
+        regex: str,
+        skipped_file_rel_paths: List[str],
+    ) -> List[str]:
+        skipped_file_rel_paths_set = set(skipped_file_rel_paths)
+        compiled_regex = re.compile(regex)
+
+        file_srcs = []
+
+        source_path = self._source_sub_path(sub_path)
+        source_sub_path = path.join(source_path, sub_path)
+        file_rel_paths = self._list_sub_path_file_rel_paths(source_sub_path)
+        file_rel_paths.sort()
+
+        for file_rel_path in file_rel_paths:
+            if compiled_regex.search(file_rel_path) is None:
+                continue
+
+            if file_rel_path in skipped_file_rel_paths_set:
+                continue
+
+            file_src = path.join(sub_path, file_rel_path)
+            file_srcs.append(file_src)
+
+        return file_srcs
+
+
+class AdbSource(Source):
+    def __init__(self):
+        self.__slot = run_cmd(
+            [
+                'adb',
+                'shell',
+                'getprop',
+                'ro.boot.slot_suffix',
+            ]
+        ).strip()
+
+    def _source_sub_path(self, sub_path: str):
+        # Files are always expected to be in the proper place on device
+        return '/'
+
+    def _copy_file_from_path(self, file_path: str, target_file_path: str):
+        try:
+            run_cmd(['adb', 'pull', file_path, target_file_path])
+            return True
+        except:
+            return False
+
+    def _list_sub_path_file_rel_paths(self, source_path: str) -> List[str]:
+        return (
+            run_cmd(
+                [
+                    'adb',
+                    'shell',
+                    f'cd {source_path}; find * -type f',
+                ]
+            )
+            .strip()
+            .splitlines()
+        )
+
+    def copy_firmware(self, file: File, target_dir: str) -> bool:
+        partition = file.root
+
+        if FileArgs.AB in file.args:
+            partition += f'_{self.__slot}'
+
+        try:
+            run_cmd(
+                ['adb', 'pull', f'/dev/block/by-name/{partition}', target_dir]
+            )
+            return True
+        except Exception:
+            return False
+
+
+class DiskSource(Source):
+    def __init__(self, source_path: str):
+        self.__source_path = source_path
+
+        # If system directory has a system subdirectory, this is System-as-Root
+        # Cache the path and use it as a source for files which are in the
+        # system sub-path
+        system_path = path.join(source_path, 'system')
+        sar_system_path = path.join(system_path, 'system')
+        if path.isdir(sar_system_path):
+            self.__system_source_path = system_path
+        else:
+            self.__system_source_path = source_path
+
+    def copy_firmware(self, file: File, target_dir: str) -> bool:
+        return self.copy_file(file, target_dir)
+
+    def _source_sub_path(self, sub_path: str):
+        if sub_path.startswith('system/'):
+            return self.__system_source_path
+
+        return self.__source_path
+
+    def _copy_file_from_path(self, file_path: str, target_file_path: str):
+        if not path.isfile(file_path):
+            return False
+
+        try:
+            shutil.copy(file_path, target_file_path)
+            return True
+        except Exception:
+            pass
+
+        return False
+
+    def _list_sub_path_file_rel_paths(self, source_sub_path: str) -> List[str]:
+        file_rel_paths = []
+
+        for dir_path, _, file_names in os.walk(source_sub_path):
+            dir_rel_path = path.relpath(dir_path, source_sub_path)
+
+            for file_name in file_names:
+                file_rel_path = path.join(dir_rel_path, file_name)
+                file_rel_path = path.normpath(file_rel_path)
+                file_rel_paths.append(file_rel_path)
+
+        return file_rel_paths
+
+
+@contextmanager
+def create_source(ctx: ExtractCtx):
+    if ctx.source == ArgsSource.ADB:
+        init_adb_connection()
+        yield AdbSource()
+        return
+
+    with get_dump_dir(ctx) as (dump_dir, extract):
+        if extract:
+            with tempfile.TemporaryDirectory() as work_dir:
+                extract_image(ctx, dump_dir, work_dir)
+
+        yield DiskSource(dump_dir)
+
+
+class CopyCtx:
+    def __init__(self, source: Source, target_dir: str):
+        self.source = source
+        self.target_dir = target_dir
+
+    def copy_file(self, file: File, is_firmware=False):
+        if is_firmware:
+            return self.source.copy_firmware(file, self.target_dir)
+
+        return self.source.copy_file(file, self.target_dir)
