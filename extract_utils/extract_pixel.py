7d4dd2dd07079b2ed9769b2ba712f5d284d0ad83
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/extract_pixel.py
@@ -0,0 +1,21 @@
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+from extract_utils.extract import ExtractCtx
+from extract_utils.tools import fbpacktool_path
+from extract_utils.utils import run_cmd
+
+
+def extract_pixel(
+    ctx: ExtractCtx,
+    file_path: str,
+    work_dir: str,
+    *args,
+    **kwargs,
+):
+    run_cmd(['python', fbpacktool_path, 'unpack', '-o', work_dir, file_path])
+    return file_path
