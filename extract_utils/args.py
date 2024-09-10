e8419cac8ef5486f05144be3c29783388ef78f95
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/args.py
@@ -0,0 +1,92 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+import argparse
+
+from enum import Enum
+from typing import Optional
+
+parser = argparse.ArgumentParser(description='Extract utils')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument(
+    '--only-common',
+    action='store_true',
+    help='only extract common module',
+)
+group.add_argument(
+    '--only-target',
+    action='store_true',
+    help='only extract target module',
+)
+# TODO: --only-firmware
+
+parser.add_argument(
+    '-n',
+    '--no-cleanup',
+    action='store_true',
+    help='do not cleanup vendor',
+)
+parser.add_argument(
+    '-k',
+    '--kang',
+    action='store_true',
+    help='kang and modify hashes',
+)
+parser.add_argument(
+    '-s',
+    '--section',
+    action='store',
+    help='only apply to section',
+)
+parser.add_argument(
+    '-r',
+    '--regenerate',
+    action='store_true',
+    help='regenerate proprietary files',
+)
+parser.add_argument(
+    '--keep-dump',
+    action='store_true',
+    help='keep the dump directory',
+)
+
+parser.add_argument(
+    'source',
+    help='sources from which to extract',
+)
+
+
+class ArgsSource(str, Enum):
+    ADB = 'adb'
+
+
+class Args:
+    def __init__(self, args: argparse.Namespace):
+        # Wrap to provide type hints
+        self.only_common: bool = args.only_common
+        self.only_target: bool = args.only_target
+        self.regenerate: bool = args.regenerate
+        self.keep_dump: bool = args.keep_dump
+        self.no_cleanup: bool = args.no_cleanup
+        self.kang: bool = args.kang
+        self.section: Optional[str] = args.section
+
+        self.source: ArgsSource | str = args.source
+        try:
+            self.source = ArgsSource(args.source)
+        except ValueError:
+            pass
+
+        if self.section is not None and self.regenerate:
+            raise ValueError('Cannot use --section with --regenerate')
+
+
+def parse_args():
+    parser_args = parser.parse_args()
+    return Args(parser_args)
