d2c1fb434b7d48b406f4c7801d486ff07bc5f369
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-10 23:37:04 +0300

    extract_utils: implement in python
    Change-Id: Iebd3587ee934303cb78ef6357596ad047d7f45be

---
+++ extract_utils/tools.py
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from __future__ import annotations
+
+from os import path
+from typing import Dict
+
+patchelf_versions = ['0_8', '0_9', '0_17_2']
+DEFAULT_PATCHELF_VERSION = '0_9'
+
+script_dir = path.dirname(path.realpath(__file__))
+android_root = path.realpath(path.join(script_dir, '..', '..', '..'))
+
+extract_utils_dir = path.realpath(path.join(script_dir, '..'))
+sdat2img_path = path.join(extract_utils_dir, 'sdat2img.py')
+
+binaries_dir = path.join(android_root, 'prebuilts/extract-tools/linux-x86/bin')
+ota_extractor_path = path.join(binaries_dir, 'ota_extractor')
+lpunpack_path = path.join(binaries_dir, 'lpunpack')
+simg2img_path = path.join(binaries_dir, 'simg2img')
+stripzip_path = path.join(binaries_dir, 'stripzip')
+
+patchelf_version_path_map: Dict[str, str] = {}
+for version in patchelf_versions:
+    patchelf_version_path_map[version] = path.join(
+        binaries_dir, f'patchelf-{version}'
+    )
+
+build_tools_dir = path.join(android_root, 'prebuilts/build-tools/linux-x86/bin')
+brotli_path = path.join(build_tools_dir, 'brotli')
+
+
+common_binaries_dir = path.join(android_root, 'prebuilts/extract-tools/common')
+apktool_path = path.join(common_binaries_dir, 'apktool/apktool.jar')
+
+jdk_binaries_dir = path.join(android_root, 'prebuilts/jdk/jdk21/linux-x86/bin')
+java_path = path.join(jdk_binaries_dir, 'java')
