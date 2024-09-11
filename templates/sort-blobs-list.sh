a466f3f3ba4ec2b140f79203d1b192a47282599f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-12 01:52:53 +0200

    templates: Add a sort-blobs-list shortcut
    Change-Id: Ia3a5bc350af0a639c404e2ae68d7572ffafe07d2

---
+++ templates/sort-blobs-list.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+# List of files to sort
+PROPRIETARY_FILES_TXT=(
+    "proprietary-files.txt"
+)
+
+# Load sort-blobs-list.py and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/sort-blobs-list.py"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+
+# Call the helper to sort the list
+# Add --dir-first to give priority to directories and subdirectories
+"${HELPER}" "${PROPRIETARY_FILES_TXT[@]}"
