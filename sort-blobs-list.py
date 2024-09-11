937ec4c34f47d2920a26010deb6f828f04f191cc
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-12 00:47:57 +0200

    extract_utils: sort-blobs-list.py: Reintroduce dir first as an opt-in
    Kinda easier to copy-paste a whole directory when updating blobs
    
    Change-Id: I195f575a9a313171c47d35240514e72d1b1fae32

--- sort-blobs-list.py
+++ sort-blobs-list.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-import re, sys
+from argparse import ArgumentParser
+import re
 from functools import cmp_to_key
 from locale import LC_ALL, setlocale, strcoll
 from pathlib import Path
@@ -25,7 +26,7 @@ def get_source_file_name(line: str) -> str:
     return line
 
 
-def strcoll_extract_utils(string1: str, string2: str) -> int:
+def strcoll_extract_utils(string1: str, string2: str, dir_first: bool = False) -> int:
     # Skip logic if one of the string if empty
     if not string1 or not string2:
         return strcoll(string1, string2)
@@ -34,6 +35,27 @@ def strcoll_extract_utils(string1: str, string2: str) -> int:
     string1 = get_source_file_name(string1)
     string2 = get_source_file_name(string2)
 
+    if dir_first:
+        # If no directories, compare normally
+        if "/" not in string1 and "/" not in string2:
+            return strcoll(string1, string2)
+
+        string1_dir = string1.rsplit("/", 1)[0] + "/"
+        string2_dir = string2.rsplit("/", 1)[0] + "/"
+        if string1_dir == string2_dir:
+            # Same directory, compare normally
+            return strcoll(string1, string2)
+
+        if string1_dir.startswith(string2_dir):
+            # First string dir is a subdirectory of the second one,
+            # return string1 > string2
+            return -1
+
+        if string2_dir.startswith(string1_dir):
+            # Second string dir is a subdirectory of the first one,
+            # return string2 > string1
+            return 1
+
     # Compare normally
     return strcoll(string1, string2)
 
@@ -41,7 +63,23 @@ def strcoll_extract_utils(string1: str, string2: str) -> int:
 if __name__ == "__main__":
     setlocale(LC_ALL, "C")
 
-    for file in sys.argv[1:] or ["proprietary-files.txt"]:
+    parser = ArgumentParser(description="Sort blobs list")
+    parser.add_argument(
+        "files",
+        nargs="*",
+        default=["proprietary-files.txt"],
+        help="Files to sort",
+    )
+    parser.add_argument(
+        "--dir-first",
+        action="store_true",
+        help="Sort directories first",
+    )
+    args = parser.parse_args()
+
+    sort_key = cmp_to_key(lambda x, y: strcoll_extract_utils(x, y, args.dir_first))
+
+    for file in args.files:
         if not Path(file).is_file():
             print(f"File {file} not found")
             continue
@@ -52,7 +90,7 @@ if __name__ == "__main__":
         ordered_sections = []
         for section in sections:
             section_list = [line.strip() for line in section.splitlines()]
-            section_list.sort(key=cmp_to_key(strcoll_extract_utils))
+            section_list.sort(key=sort_key)
             ordered_sections.append("\n".join(section_list))
 
         with open(file, "w") as f:
