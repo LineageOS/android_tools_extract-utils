LineageOS/android_tools_extract-utils

commit 2a10a4dbfbd7e99d9ab28d8227fd5871c9d446eb

extract_utils: Allow SYMLINK co-exist with other args
Change-Id: I2ce1d19f3f5f5b17fd61f467e4e390e30800c9f8

extract_utils.sh
@@ -236,11 +236,21 @@ function prefix_match() {
     for ((i = 1; i < COUNT + 1; i++)); do
         local FILE="${DEST_LIST[$i - 1]}"
         if [[ "$FILE" =~ ^"$PREFIX" ]]; then
-            local ARGS="${ARGS_LIST[$i - 1]}"
-            if [[ -z "${ARGS}" || "${ARGS}" =~ 'SYMLINK' ]]; then
+            local SPEC_ARGS="${ARGS_LIST[$i - 1]}"
+            local ARGS=(${SPEC_ARGS//;/ })
+            local FILTERED_ARGS=()
+
+            for ARG in "${ARGS[@]}"; do
+                if [[ "$ARG" =~ ^SYMLINK= ]]; then
+                    continue
+                fi
+                FILTERED_ARGS+=("$ARG")
+            done
+
+            if [ ${#FILTERED_ARGS[@]} -eq 0 ]; then
                 NEW_ARRAY+=("${FILE#"$PREFIX"}")
             else
-                NEW_ARRAY+=("${FILE#"$PREFIX"};${ARGS}")
+                NEW_ARRAY+=("${FILE#"$PREFIX"};${FILTERED_ARGS}")
             fi
         fi
     done

commit 8571091025c39d6028d72cf524c9f67d101fd553

extract-utils: Join filtered args before adding them to the array
Fixes regression introduced by 2a10a4d.

Change-Id: I0ee57025f51b1e8ca944575ca74b1576f065b0fb

extract_utils.sh
@@ -247,11 +247,8 @@ function prefix_match() {
                 FILTERED_ARGS+=("$ARG")
             done
 
-            if [ ${#FILTERED_ARGS[@]} -eq 0 ]; then
-                NEW_ARRAY+=("${FILE#"$PREFIX"}")
-            else
-                NEW_ARRAY+=("${FILE#"$PREFIX"};${FILTERED_ARGS}")
-            fi
+            FILTERED_ARGS=$(IFS=";"; echo "${FILTERED_ARGS[@]}")
+            NEW_ARRAY+=("${FILE#"$PREFIX"};${FILTERED_ARGS}")
         fi
     done
     printf '%s\n' "${NEW_ARRAY[@]}" | LC_ALL=C sort