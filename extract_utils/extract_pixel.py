ebe8f8148093336c6023ff909a7315adb1575700
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-21 00:18:50 +0300

    extract_utils: add extract function for copying pixel fimware
    Pixel firmware comes with a release-dependent file name.
    Add method for renaming it to a simpler file name to ease integration.
    
    Eg:
    bootloader-akita-akita-15.0-12059390.img
    ->
    bootloader.img
    
    Change-Id: If149b91f01f65cc1017af551bf518c2671aedcf0

--- extract_utils/extract_pixel.py
+++ extract_utils/extract_pixel.py
@@ -5,9 +5,32 @@
 
 from __future__ import annotations
 
+import shutil
+from os import path
+
 from extract_utils.extract import ExtractCtx
 from extract_utils.tools import fbpacktool_path
 from extract_utils.utils import run_cmd
+
+
+def copy_pixel_firmware(
+    ctx: ExtractCtx,
+    file_path: str,
+    work_dir: str,
+    *args,
+    **kwargs,
+):
+    file_name = path.basename(file_path)
+    file_root, ext = path.splitext(file_name)
+
+    # Remove anything after (and including) the first dash
+    simple_file_root = file_root.split('-', 1)[0]
+
+    output_file_name = f'{simple_file_root}{ext}'
+    output_file_path = path.join(work_dir, output_file_name)
+
+    shutil.copy(file_path, output_file_path)
+    return file_path
 
 
 def extract_pixel_firmware(
