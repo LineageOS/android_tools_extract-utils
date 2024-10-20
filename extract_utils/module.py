06ab6bd6ef115546d137209b084253b24f35f60d
Alexander Koskovich <akoskovich@pm.me>
2024-10-20 08:12:37 -0400

    extract_utils: Allow passing custom device_rel_path for modules
    Having the device_path generated based on the vendor and device
    calculated breaks the below scenarios:
    
    device/google/caimito/caiman/extract-files.py
    device/google/caimito/komodo/extract-files.py
    device/google/caimito/tokay/extract-files.py
    
    Change-Id: I4c8b1efba2fcf89a4ff5fefa779ee67eac8a41cc

--- extract_utils/module.py
+++ extract_utils/module.py
@@ -268,6 +268,7 @@ class ExtractUtilsModule:
         self,
         device,
         vendor,
+        device_rel_path: Optional[str] = None,
         blob_fixups: Optional[blob_fixups_user_type] = None,
         lib_fixups: Optional[lib_fixups_user_type] = None,
         namespace_imports: Optional[List[str]] = None,
@@ -293,7 +294,10 @@ class ExtractUtilsModule:
 
         self.check_elf = check_elf
 
-        self.device_rel_path = path.join('device', vendor, device)
+        if device_rel_path is None:
+            device_rel_path = path.join('device', vendor, device)
+
+        self.device_rel_path = device_rel_path
         self.device_path = path.join(android_root, self.device_rel_path)
 
         self.vendor_rel_path = path.join('vendor', vendor, device)
