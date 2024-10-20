dba79a918eb79889751d491d277b596a966bda3b
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-20 22:32:19 +0300

    extract_utils: add script for converting to python dump structure
    Change-Id: I43bbd76cd76edd01efc77a1828b5c8344c6a9c54

--- extract_utils/extract.py
+++ extract_utils/extract.py
@@ -683,6 +683,10 @@ def extract_image(source: str, ctx: ExtractCtx, dump_dir: str):
             processed_file = extract_fn(ctx, file.path, dump_dir)
             remove_file_paths([processed_file])
 
+    move_alternate_partition_paths(dump_dir)
+
+
+def move_alternate_partition_paths(dump_dir: str):
     # Make sure that even for devices that don't have separate partitions
     # for vendor, odm, etc., the partition directories are copied into the root
     # dump directory to simplify file copying
@@ -712,6 +716,18 @@ def extract_image(source: str, ctx: ExtractCtx, dump_dir: str):
 
         shutil.move(system_dir, system_root_dir)
         shutil.move(system_root_system_dir, dump_dir)
+
+
+def convert_dump(dump_dir: str):
+    dump_output_dir = path.join(dump_dir, 'output')
+
+    if path.isdir(dump_output_dir):
+        for file in os.scandir(dump_output_dir):
+            shutil.move(file.path, dump_dir)
+
+        shutil.rmtree(dump_output_dir)
+
+    move_alternate_partition_paths(dump_dir)
 
 
 def filter_already_extracted_partitions(dump_dir: str, ctx: ExtractCtx):
