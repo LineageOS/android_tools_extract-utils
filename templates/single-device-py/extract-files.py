3fa2621ad164e24da1d460828b90f7044f139365
LuK1337 <priv.luk@gmail.com>
2024-10-21 15:09:02 +0200

    extract_utils: Add libs_proto_21_12 example to templates
    Change-Id: Ic3bd2c6474bc86e3ca83a8eacbb1abddebadc6bc

--- templates/single-device-py/extract-files.py
+++ templates/single-device-py/extract-files.py
@@ -13,10 +13,12 @@ from extract_utils.fixups_blob import (
 from extract_utils.fixups_lib import (
     lib_fixup_remove,
     lib_fixup_remove_arch_suffix,
+    lib_fixup_remove_proto_version_suffix,
     lib_fixup_vendorcompat,
     lib_fixups_user_type,
     libs_clang_rt_ubsan,
     libs_proto_3_9_1,
+    libs_proto_21_12,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -35,6 +37,7 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
     libs_proto_3_9_1: lib_fixup_vendorcompat,
+    libs_proto_21_12: lib_fixup_remove_proto_version_suffix,
     (
         'vendor.twopac.hardware.xoo@1.0',
         'vendor.twopac.hardware.oxo@1.0',
