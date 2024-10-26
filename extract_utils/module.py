f40a1bb1668fdc38623780699d23cef355091271
Arian <arian.kulmer@web.de>
2024-10-26 12:31:49 +0200

    extract_utils: Don't filter firmware by section for makefile writing
    Firmware makefile writing used to rely on `files` directly which is
    filtered by sections. However when extracting one section, the
    makefiles need to be rewritten and thus we need to keep track of all
    firmware files too in order to not write makefiles without them.
    
    Change-Id: I535b1f1c68fb77d57727e0bd89be16a41b4e11bb

--- extract_utils/module.py
+++ extract_utils/module.py
@@ -168,6 +168,7 @@ class ProprietaryFile:
         self.file_list = FileList(
             section=section,
             check_elf=module.check_elf,
+            is_firmware=isinstance(self, FirmwareProprietaryFile)
         )
 
     def parse(self):
@@ -192,7 +193,7 @@ class FirmwareProprietaryFile(ProprietaryFile):
 
     def write_makefiles(self, module: ExtractUtilsModule, ctx: MakefilesCtx):
         write_mk_firmware_ab_partitions(
-            self.file_list.files,
+            self.file_list.firmware_files,
             ctx.board_config_mk_out,
         )
 
@@ -201,7 +202,7 @@ class FirmwareProprietaryFile(ProprietaryFile):
         write_mk_firmware(
             module.vendor_path,
             self.vendor_rel_sub_path,
-            self.file_list.files,
+            self.file_list.firmware_files,
             ctx.mk_out,
         )
 
