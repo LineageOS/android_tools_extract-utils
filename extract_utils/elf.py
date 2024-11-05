536506da64ced80fbef93f585e3c67d391336c72
Cosmin Tanislav <demonsingur@gmail.com>
2024-11-05 18:46:31 +0200

    extract_utils: roll our own ELF parser
    Change-Id: I490ded0341895f93c2ac1656605b27dc2ea85b08

--- extract_utils/elf.py
+++ extract_utils/elf.py
@@ -5,42 +5,19 @@
 from __future__ import annotations
 
 from contextlib import suppress
-from typing import Generator, Iterable, List, Optional, Tuple
+from typing import Iterable, List, Optional, Tuple
 
-from elftools.common.exceptions import ELFError
-from elftools.elf.dynamic import DynamicSection
-from elftools.elf.elffile import ELFFile
+from extract_utils.elf_parser import EM, ELFError, ELFFile
 
 SO_SUFFIX = '.so'
 SO_SUFFIX_LEN = len(SO_SUFFIX)
-
-
-def __get_elf_libs(elf: ELFFile) -> Generator[str, None, None] | None:
-    section = elf.get_section_by_name('.dynamic')
-    if not section:
-        return None
-
-    assert isinstance(section, DynamicSection)
-
-    for t in section.iter_tags():
-        if not t.entry.d_tag == 'DT_NEEDED':
-            continue
-
-        # TODO: figure out proper typing for this
-        needed = t.needed  # type: ignore
-        assert isinstance(needed, str)
-        yield needed
 
 
 def file_needs_lib(file_path: str, needed_lib: str) -> bool:
     with suppress(ELFError):
         with open(file_path, 'rb') as f:
             elf = ELFFile(f)
-            libs = __get_elf_libs(elf)
-
-            if libs is None:
-                return False
-
+            libs = elf.get_libs()
             return any(lib == needed_lib for lib in libs)
 
     return False
@@ -60,20 +37,22 @@ def remove_libs_so_ending(libs: None | Iterable[str]) -> None | List[str]:
 
 
 def get_file_machine_bits_libs(
-    file_path: str, get_libs: bool
-) -> Tuple[str, int, Optional[List[str]]] | Tuple[None, None, None]:
+    file_path: str, gen_deps: bool
+) -> Tuple[EM, int, Optional[List[str]]] | Tuple[None, None, None]:
     try:
         with open(file_path, 'rb') as f:
             elf = ELFFile(f)
+            machine = elf.machine
+            bits = elf.bits
             libs = None
 
-            if get_libs:
-                libs = __get_elf_libs(elf)
+            if gen_deps:
+                libs = elf.get_libs()
 
             if libs is not None:
                 libs = list(libs)
 
-            return elf['e_machine'], elf.elfclass, libs
+            return machine, bits, libs
     except ELFError:
         return None, None, None
 
