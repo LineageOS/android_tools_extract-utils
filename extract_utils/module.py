785f14124f1d64274840375f92255ae90059d60d
Cosmin Tanislav <demonsingur@gmail.com>
2024-11-05 21:53:50 +0200

    extract_utils: enable ELF checks by default
    Change-Id: I09db3f75b6b4e2a8f56d005fb5daac013f2b868b

--- extract_utils/module.py
+++ extract_utils/module.py
@@ -317,7 +317,7 @@ class ExtractUtilsModule:
         lib_fixups: Optional[lib_fixups_user_type] = None,
         namespace_imports: Optional[List[str]] = None,
         extract_fns: Optional[extract_fns_user_type] = None,
-        check_elf=False,
+        check_elf=True,
         add_firmware_proprietary_file=False,
         add_generated_carriersettings_file=False,
         add_generated_carriersettings=False,
@@ -493,6 +493,12 @@ class ExtractUtilsModule:
             )
 
     def write_makefiles(self, legacy: bool):
+        if not self.check_elf:
+            color_print(
+                'check_elf = False is deprecated and will be removed in Android 16',
+                color=Color.YELLOW,
+            )
+
         bp_path = path.join(self.vendor_path, 'Android.bp')
         mk_path = path.join(self.vendor_path, 'Android.mk')
         product_mk_path = path.join(
