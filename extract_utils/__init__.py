46610e4fc4297c37ca4f9ba059f1908e98190474
LuK1337 <priv.luk@gmail.com>
2024-11-05 10:24:32 +0100

    extract_utils: exit early if elftools is not installed
    Change-Id: I23968a30d296e62af3d5747f5646a8801e3a2aaf

--- extract_utils/__init__.py
+++ extract_utils/__init__.py
@@ -3,6 +3,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+try:
+    import elftools
+except ImportError:
+    sys.exit(
+        f"""
+pyelftools is not found, please install it by using one of the following commands:
+* apk add py3-elftools
+* apt install python3-pyelftools
+* dnf install python3-pyelftools
+* emerge dev-python/pyelftools
+* pacman -S python-pyelftools
+* zypper install python3-pyelftools
+* pip install pyelftools
+""".strip()
+    )
+
 import sys
 
 sys.dont_write_bytecode = True
