fd59f11f1d49946ae9abff35261b5e5b55080888
Cosmin Tanislav <demonsingur@gmail.com>
2024-11-07 18:52:14 +0200

    extract_utils: allow generation of apns along carrier settings
    Change-Id: Ic98c8649ea6342faafaf24f8c22ab05e927d1231

--- extract_utils/postprocess.py
+++ extract_utils/postprocess.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 
 import os
-from typing import Callable
+from typing import Callable, Optional
 
 from extract_utils.tools import carriersettings_extractor_path
 from extract_utils.utils import run_cmd
@@ -21,9 +21,16 @@ postprocess_fn_type = Callable[[PostprocessCtx], None]
 
 
 def postprocess_carriersettings_fn_impl(
-    input_path: str, output_path: str, ctx: PostprocessCtx
+    input_path: str,
+    output_path: str,
+    ctx: PostprocessCtx,
+    apn_output_path: Optional[str] = None,
 ):
     os.makedirs(output_path, exist_ok=True)
+
+    apn_args = []
+    if apn_output_path is not None:
+        apn_args = ['-a', apn_output_path]
 
     run_cmd(
         [
@@ -33,4 +40,5 @@ def postprocess_carriersettings_fn_impl(
             '-v',
             output_path,
         ]
+        + apn_args
     )
