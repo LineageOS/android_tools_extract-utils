5b995fabaa869a5036cfc9275cd10e6a205d944d
LuK1337 <priv.luk@gmail.com>
2024-11-08 13:16:11 +0100

    extract_utils: allow `call()` to accept arbitrary arguments
    Change-Id: I258116acd5971f88a014f50a2222e1df1fdd4145

--- extract_utils/fixups_blob.py
+++ extract_utils/fixups_blob.py
@@ -57,8 +57,9 @@ class blob_fixup:
         self,
         fn: blob_fixup_fn_impl_type,
         need_tmp_dir=True,
+        *args,
     ) -> Self:
-        self.__functions.append(fn)
+        self.__functions.append((fn, args))
         if need_tmp_dir:
             self.__create_tmp_dir = True
         return self
@@ -436,8 +437,8 @@ class blob_fixup:
 
     def run(self, ctx: BlobFixupCtx, file: File, file_path: str) -> bool:
         def run(tmp_dir: str | None = None):
-            for function in self.__functions:
-                function(ctx, file, file_path, tmp_dir=tmp_dir)
+            for function, args in self.__functions:
+                function(ctx, file, file_path, *args, tmp_dir=tmp_dir)
 
         if self.__create_tmp_dir:
             with tempfile.TemporaryDirectory() as tmp_dir:
