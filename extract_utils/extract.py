f7ca38747d264e8d14e3be7c287472f15adff440
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-02 13:52:42 +0100

    extract_utils: Ensure source dir is writable before using it
    Change-Id: I128845ede4f76bef74023934a5b36cc0947471ce

--- extract_utils/extract.py
+++ extract_utils/extract.py
@@ -595,9 +595,11 @@ class DumpDirResult:
         self,
         dump_dir: str,
         copy_source_dir_to_dump_dir=False,
+        source_dir_not_writeable=False,
     ):
         self.dump_dir = dump_dir
         self.copy_source_dir_to_dump_dir = copy_source_dir_to_dump_dir
+        self.source_not_writeable = source_dir_not_writeable
 
 
 @contextmanager
@@ -630,7 +632,10 @@ def get_dump_dir(
     if path.isdir(source):
         # Source is a directory, try to extract its contents into itself
         print(f'Extracting to source dump dir {source}')
-        yield DumpDirResult(dump_dir)
+        yield DumpDirResult(
+            dump_dir,
+            source_dir_not_writeable=not os.access(source, os.W_OK),
+        )
         return
 
     # Remove the extension from the file and use it as a dump dir
