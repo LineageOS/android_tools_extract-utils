89aa209a1c40815fb5cb62b7e316c1a596bf58cf
Alexander Koskovich <akoskovich@pm.me>
2025-05-13 18:25:19 -0400

    Let user know if command being used isn't found
    Change-Id: Iadec645673a09eb08040427cc69e2365f81cf28f

--- extract_utils/utils.py
+++ extract_utils/utils.py
@@ -109,7 +109,11 @@ def executable_path(name: str) -> str:
 
 
 def run_cmd(cmd: List[str], shell=False):
-    cmd[0] = executable_path(cmd[0])
+    try:
+        cmd[0] = executable_path(cmd[0])
+    except ValueError:
+        color_print(f'{cmd[0]}: command not found', color=Color.RED)
+
     proc = run(
         cmd,
         stdout=PIPE,
