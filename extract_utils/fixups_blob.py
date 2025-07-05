c19d16aa8b496f58c0959d6a55c89e763a0848fc
bengris32 <bengris32@protonmail.ch>
2025-07-05 16:51:14 +0100

    extract_utils: Avoid passing non existent paths to git add
    Some patches may add, delete or rename files instead of modifying
    them in-place. In such cases, non existent paths may be passed to
    git add.
    
    Filter out non existent paths out of affected_files before passing
    them to git add.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0bd826f5481c8e9cb35981e234d40a912dc36301

--- extract_utils/fixups_blob.py
+++ extract_utils/fixups_blob.py
@@ -257,7 +257,9 @@ class blob_fixup:
         affected_files = self.__get_patches_affected_files(patches)
 
         def git_add_files():
-            run_cmd(['git', 'add'] + affected_files)
+            run_cmd(
+                ['git', 'add'] + [f for f in affected_files if path.isfile(f)]
+            )
 
         # Try to apply the changes in reverse, so that they apply cleanly
         # forward
