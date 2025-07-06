ab540973dc91d6b20d9b0075d0c67507f22490c5
LuK1337 <priv.luk@gmail.com>
2025-07-06 12:25:10 +0200

    extract_utils: use `apply` instead of `am`
    Fixes applying diffs without git metadata.
    
    Change-Id: I3f55e5819790d0f4620ef5614afd49d20b817f32

--- extract_utils/fixups_blob.py
+++ extract_utils/fixups_blob.py
@@ -284,7 +284,8 @@ class blob_fixup:
 
             for patch in patches:
                 try:
-                    run_cmd(['git', 'am', '--reject', patch])
+                    run_cmd(['git', 'apply', '--reject', patch])
+                    run_cmd(['git', 'commit', '-m', f'Apply: "{patch}"'])
                 except ValueError as e:
                     color_print(
                         f'Failed to apply patch {patch}',
