7395a598ddcc7e9dd5c5e42711ef391f1c8cf572
LuK1337 <priv.luk@gmail.com>
2025-07-07 10:48:05 +0200

    extract_utils: use cwd='/' for getting the list of affected files
    `git apply --numstat` returns different paths depending on current
    working directory:
    
    $ pwd
    /home/luk/lineage-22/device/google/bramble
    $ git apply --numstat /home/luk/lineage-22/device/google/bramble/bramble/HbmSVManager.patch
    46	2	smali/com/android/hbmsvmanager/filter/AmbientFilterFactory.smali
    $ pwd
    /home/luk/lineage-22/device/google/bramble/bramble
    $ git apply --numstat /home/luk/lineage-22/device/google/bramble/bramble/HbmSVManager.patch
    46	2	bramble/smali/com/android/hbmsvmanager/filter/AmbientFilterFactory.smali
    
    And because of that __get_apktool_unpack_args() fails due to expecting
    path to start with res/, smali/ or be equal to AndroidManifest.xml.
    
    Change-Id: I947c115d61207d7be8280939d8266adda9f49a16

--- extract_utils/fixups_blob.py
+++ extract_utils/fixups_blob.py
@@ -192,7 +192,7 @@ class blob_fixup:
         return patches
 
     def __get_patch_affected_files(self, patch: str) -> List[str]:
-        output = run_cmd(['git', 'apply', '--numstat', patch])
+        output = run_cmd(['git', 'apply', '--numstat', patch], cwd='/')
 
         files = []
         for line in output.strip().splitlines():
