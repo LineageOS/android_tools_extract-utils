a1de3505ba7de91f536e1ef9f8826f52b1a36215
Nolen Johnson <johnsonnolen@gmail.com>
2026-04-01 01:08:14 -0400

    extract: Prohibit specific files from being extracted
    * Known DMCA risks.
    
    Change-Id: I64d683b49b27537b4b87af80840561da7197a6dc

--- extract_utils/module.py
+++ extract_utils/module.py
@@ -50,6 +50,7 @@ from extract_utils.postprocess import (
     postprocess_carriersettings_fn_impl,
     postprocess_fn_type,
 )
+from extract_utils.prohibited_files import fail_prohibited, is_prohibited
 from extract_utils.source import DiskSource, Source
 from extract_utils.tools import android_root
 from extract_utils.utils import (
@@ -258,6 +259,14 @@ class ProprietaryFile:
     def parse(self):
         assert self.file_list_path is not None
         self.file_list.add_from_file(self.file_list_path)
+
+        prohibited_files: List[str] = []
+        for file in self.file_list.files:
+            for file_path in {file.src, file.dst}:
+                if is_prohibited(file_path):
+                    prohibited_files.append(file_path)
+
+        fail_prohibited(sorted(set(prohibited_files)))
 
     def get_files(self) -> Iterable[File]:
         return self.file_list.files
