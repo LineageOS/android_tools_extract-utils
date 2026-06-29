377e9593691db06d77648ff30b3fc0ac43783072
Cosmin Tanislav <demonsingur@gmail.com>
2026-06-29 13:18:57 +0300

    extract_utils: add support for exclude_shared_libs on prebuilt cc modules
    Change-Id: Iedbd04bbd3cbe7cc9a9c6ab330b619428419b3a3

--- templates/single-device/extract-files.py
+++ templates/single-device/extract-files.py
@@ -11,6 +11,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_exclude,
     lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
@@ -43,6 +44,10 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.twopac.hardware.oox@1.0',
     ): lib_fixup_vendor_suffix,
     'libwpa_client': lib_fixup_remove,
+    (
+        'android.media.audio.common.types-V3-ndk',
+        'android.hardware.audio.core-V2-ndk',
+    ): lib_fixup_exclude,
 }
 
 
