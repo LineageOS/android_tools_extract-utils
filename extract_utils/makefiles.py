38590c5fc38b3f7fb082d388c774b8a3f78f76d7
Tuan Anh <tuan73176@gmail.com>
2026-09-15 13:27:20 +0700

    extract_utils: Allow generating blueprint rules for kernel modules
    Change-Id: I7b3b8ad987d8f610f5fb8dfbc50e5f5c6caa410f

--- extract_utils/makefiles.py
+++ extract_utils/makefiles.py
@@ -35,6 +35,9 @@ ALL_PARTITIONS = [
     'system_ext',
     'odm',
     'recovery',
+    'vendor_dlkm',
+    'odm_dlkm',
+    'system_dlkm',
 ]
 APEX_PARTITIONS = ['system', 'vendor', 'system_ext']
 RFSA_PARTITIONS = ['vendor', 'odm']
@@ -332,6 +335,21 @@ def write_rfsa_package(file: File, builder: FileBpBuilder):
     return package_name
 
 
+def write_kernel_module_package(file: File, builder: FileBpBuilder):
+    _, package_name = file_stem_package_name(
+        file,
+        can_have_stem=True,
+    )
+
+    (
+        builder.set_rule_name('prebuilt_kernel_modules')
+        .name(package_name)
+        .srcs()
+        .specific()
+    )
+    return package_name
+
+
 def write_apex_package(file: File, builder: FileBpBuilder):
     _, package_name = file_stem_package_name(file)
 
@@ -538,6 +556,11 @@ def write_product_packages(
     for part in ALL_PARTITIONS:
         part_path = get_part_path(part)
 
+        # Extract kernel modules first so that they don't end up in lib32
+        kernel_modules_tree = base_file_tree.filter_prefixed(
+            part_path + ['lib', 'modules']
+        )
+
         lib_rfsa_tree = None
         if part in RFSA_PARTITIONS:
             # Extract these first so that they don't end up in lib32
@@ -565,6 +588,8 @@ def write_product_packages(
 
         if lib_rfsa_tree is not None:
             w(write_rfsa_package, lib_rfsa_tree)
+
+        w(write_kernel_module_package, kernel_modules_tree)
 
     for part in APEX_PARTITIONS:
         wp(write_apex_package, part, 'apex')
